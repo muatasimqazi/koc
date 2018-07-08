dfa1709d858dc76e3fff9592c1b4d6a28165e15c
Muatasim Qazi <muatasimqazi@gmail.com>
2018-07-08 01:19:17 -0700

    sign up view

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,7 @@ import { Container, Row, Col } from 'react-grid-system';
 import SimpleCard from './SimpleCard';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
 
 const styles = {
     container: {
@@ -31,18 +32,55 @@ const styles = {
         fontSize: '1.5em',
         marginRight: 10,
         verticalAlign: 'middle'
+    },
+    divider: {
+        borderBottom: '1px solid #ebeef1',
+        display: 'inline-block',
+        width: '40%'
+
     }
 }
 class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            password: '',
+        }
+    }
+
+    handleChange = name => event => {
+        this.setState({
+            [name]: event.target.value,
+        });
+    };
     render() {
         return (
-            <Container debug>
+            <Container>
                 <Row style={styles.container}>
                     <Col sm={6}>
-                        <SimpleCard title="Welcome Back" button="Login">
+                        <SimpleCard title="Welcome Back">
                             <Paper style={{ ...styles.social, ...styles.facebook }} elevation={0}><span style={styles.icon} className="fab fa-facebook-square"></span>Login with Facebook</Paper>
                             <Paper style={{ ...styles.social, ...styles.google }} elevation={0}><span style={styles.icon} className="fab fa-google-plus-square"></span>Login with Google</Paper>
-                            <Button size="medium" variant="contained" color="primary">Login</Button>
+                            <div style={{ textAlign: 'center', padding: 10, color: 'grey' }}><span style={styles.divider}></span> or <span style={styles.divider}></span></div>
+                            <form>
+                                <TextField
+                                    id="username"
+                                    label="Username or email address"
+                                    value={this.state.username}
+                                    margin="normal"
+                                    fullWidth
+                                />
+                                <TextField
+                                    id="password"
+                                    label="Password"
+                                    value={this.state.password}
+                                    margin="normal"
+                                    fullWidth
+                                />
+                                <Button size="large" variant="contained" color="primary" style={{marginTop: 20}}>Login</Button>
+                            </form>
                             <p><a href="/">Lost Password</a></p>
                         </SimpleCard>
                     </Col>
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,11 +1,124 @@
 import React, { Component } from 'react';
+import SimpleCard from './SimpleCard';
+import { Container, Row, Col } from 'react-grid-system';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
 
+const styles = {
+    container: {
+        display: 'flex',
+        justifyContent: 'center'
+    },
+    item: {
+    },
+    social: {
+        padding: '6px 0',
+        height: '2.5rem',
+        textAlign: 'center',
+        color: '#fff',
+        fontWeight: 200,
+        borderRadius: 3,
+        marginBottom: 10,
+        lineHeight: '2.5rem'
+    },
+    facebook: {
+        background: '#3b5998',
+    },
+    google: {
+        background: '#d34836',
+    },
+    icon: {
+        fontSize: '1.5em',
+        marginRight: 10,
+        verticalAlign: 'middle'
+    }
+}
+
+
+const cities = [
+    {
+      name: 'San Francisco',
+    },
+    {
+        name: 'Seattle',
+    },
+]
 class SignUp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            city: ''
+        }
+    } 
+
+    handleChange = name => event => {
+        this.setState({
+            [name]: event.target.value,
+        });
+    };
+
     render() {
         return (
-            <div>
-                <h1>SignUp</h1>
-            </div>
+            <Container>
+                <Row style={styles.container}>
+                    <Col sm={10}>
+                        <SimpleCard title="Sign Up">
+                            <form>
+                                <TextField
+                                    id="name"
+                                    label="Your Name"
+                                    placeholder="Your full name"
+                                    value={this.state.name}
+                                    margin="normal"
+                                    fullWidth
+                                />
+                                <TextField
+                                    id="email"
+                                    label="Your email"
+                                    placeholder="you@yourdomain.com"
+                                    value={this.state.name}
+                                    margin="normal"
+                                    fullWidth
+                                />
+                                <TextField
+                                    id="password"
+                                    label="Password"
+                                    value={this.state.name}
+                                    margin="normal"
+                                    fullWidth
+                                />
+                                <TextField
+                                    id="verifyPassword"
+                                    label="Verify Password"
+                                    value={this.state.name}
+                                    margin="normal"
+                                    fullWidth
+                                />
+                                <TextField
+                                id="select-city"
+                                select
+                                label="Select"
+                                value={this.state.city}
+                                onChange={this.handleChange('city')}
+                                helperText="Please select your city"
+                                margin="normal"
+                                >
+                                {cities.map(option => (
+                                    <MenuItem key={option.name} value={option.name}>
+                                    {option.name}
+                                    </MenuItem>
+                                ))}
+                                </TextField>
+
+                            </form>
+                            <Button size="large" variant="contained" color="primary" style={{marginTop: 20}}>Sign Up</Button>
+                        </SimpleCard>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
